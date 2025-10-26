5f7150ea675758c9b1abfc1a7a4acfb8e97a8372
Nick Hildebrandt <N.Hildebrandt@ts13.de>
2025-10-26 22:39:12 +0100

    Integrate PWA support with Vite PWA plugin and update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+	import { pwaInfo } from 'virtual:pwa-info';
+
+  	let webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : ''
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+ 	{@html webManifestLink}
 </svelte:head>
 
 {@render children?.()}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
 import tailwindcss from '@tailwindcss/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			includeAssets: [
+				'favicon.ico',
+				'robots.txt',
+				'apple-touch-icon.png',
+				'pwa-192x192.png',
+				'pwa-512x512.png',
+				'pwa-512x512-maskable.png'
+			],
+			manifest: {
+				name: 'Ts-Note',
+				short_name: 'TSN',
+				description: 'A simple, lightweight note-taking web app written entirely in SvelteKit, featuring advanced tools like note timers.',
+				theme_color: '#06B6D4',
+				background_color: '#10B981',
+				display: 'standalone',
+				start_url: '/',
+				icons: [
+					{
+						src: '/pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					},
+					{
+						src: '/pwa-512x512-maskable.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable'
+					}
+				]
+			},
+			workbox: {
+				globDirectory: '.svelte-kit/output',
+				globPatterns: ['client/**/*.{js,css,html,ico,png,svg,webp,webmanifest,json}'],
+				globIgnores: ['server/**'],
+				modifyURLPrefix: {}
+			}
+    	})
+	]
 });
